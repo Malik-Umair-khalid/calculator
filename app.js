f604096dc142c2ad1bb60366ae4cd5fe947197c1
Malik Umair Khalid <fighteromega3462@gmail.com>
2021-06-12 02:15:06 +0500

    completrd

---
+++ app.js
@@ -0,0 +1,11 @@
+
+    function calc(num){
+        var data = document.getElementById('screen').value += num
+        return "screen"
+    } 
+    function math(operation){
+        operation.target(onclick= document.getElementById('screen').value = eval(document.getElementById('screen').value))
+    }
+    // function clear(operation){
+    //     operation.target(onclick(document.getElementById('screen').value = ""))
+    // }
