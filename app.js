9623f97b3788166385ff29ad6412d12697a2a2c5
Malik Umair Khalid <fighteromega3462@gmail.com>
2021-06-15 14:59:21 +0500

    porra ho gia

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 
     function calc(num){
-        var data = document.getElementById('screen').value += num
-        return "screen"
+        var data = document.getElementById('screen')
+        var lastvalue = data.value.slice(-1)
+        var operations = ['+','-','*','/',]
+        // agar calculator ko first condition na 1 milla to is ka matlab ka user na koi number
+        //  press kia ha kion ka aghar wo operation press karta to us ko opeeratin walee array 
+        // sa wo operation mill jata aur condition !== Walee sahi ho jai gi.
+        // && ka bad walee condition ma agar user ka press kia hoa buttion number hoga to condition -1  ho gae gi kio ka number hoga
+        // ayr number ki value us ko operation walee array ma nahi milla gi jab ka operation us ko operation walay array ma mill gai ga to condition true ho jae gi
+        if (operations.indexOf(lastvalue) !== -1 && operations.indexOf(num) !== -1){
+            data.value = data.value.toString().slice(0, -1) + num
+        }
+        else{
+            data.value += num 
+        }
+        console.log(lastvalue)
     } 
     function math(operation){
-        operation.target(onclick= document.getElementById('screen').value = eval(document.getElementById('screen').value))
+        var data = document.getElementById('screen')
+        data.value = eval(data.value)
     }
-    // function clear(operation){
-    //     operation.target(onclick(document.getElementById('screen').value = ""))
-    // }
+    function del(){
+        var data = document.getElementById('screen')
+        data.value = data.value.toString().slice(0, -1)
+    }
+    function allClear(){
+        var data = document.getElementById('screen')
+        data.value = ''
+    }
